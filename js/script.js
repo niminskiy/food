c57cc33f2e61d0a73777e6a445cd49e0f93b467b
niminskiy <s.niminskiy@gmail.com>
2020-11-29 03:10:44 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -377,11 +377,15 @@ window.addEventListener('DOMContentLoaded', () => {
     dots.push(dot);
   }
 
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
+
   next.addEventListener('click', () => {
-    if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
         offset = 0;
     } else {
-        offset += +width.slice(0, width.length - 2); 
+        offset += deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -404,9 +408,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   prev.addEventListener('click', () => {
     if (offset == 0) {
-        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-        offset -= +width.slice(0, width.length - 2);
+        offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -430,7 +434,7 @@ window.addEventListener('DOMContentLoaded', () => {
   dots.forEach(dot => {
     dot.addEventListener('click', (e) => {
       const slideTo = e.target.getAttribute('data-slide-to');
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = deleteNotDigits(width) * (slideTo - 1);
 
       slideIndex = slideTo;
 
